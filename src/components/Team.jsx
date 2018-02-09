a801edab6533265a7cdb2fe0ce8ff4095ef3b655
Malgorzata <gosia.haniszewska@gmail.com>
2018-02-09 13:43:02 -0800

    add TeamsList and Team components

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import BaseballSymbol from '../assets/images/symbol.jpg';
 
-function Team(){
+function Team(props){
   return (
-    <div>
+    <div style={{padding: '50px'}} className="col-md-4">
+      <div className="row">
+        <div className="col-md-4">
+          <img style={{width: '90px'}} src={BaseballSymbol} alt="symbol"></img>
+        </div>
+        <div className="col-md-8">
+          <br></br>
+          <h3>{props.name}</h3>
+        </div>
+      </div>
+      <br></br>
+      <div>
+        <h5>Manager: {props.manager}</h5>
+        <h5>Players: </h5>
+        <ul>
+          {props.players.map((player, i) =>
+            <li>{player}</li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
+
+Team.propTypes = {
+  name: PropTypes.string,
+  manager: PropTypes.string,
+  players: PropTypes.arrayOf(PropTypes.string)
+};
 
 export default Team;
--- src/components/TeamsList.jsx
+++ src/components/TeamsList.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
+import Team from './Team';
+
+let masterTeamsList= [
+  {
+    name: 'Expos',
+    manager: 'Kevin',
+    players: ['Kevin', 'Kam', 'Brian']
+  },
+  {
+    name: 'Tigers',
+    manager: 'Byron',
+    players: ['Mike', 'Gosia', 'Tom']
+  },
+  {
+    name: 'Braves',
+    manager: 'Eric',
+    players: ['Adam', 'Paul', 'Danka']
+  },
+  {
+    name: 'Brewers',
+    manager: 'Nat',
+    players: ['Nat', 'Sam', 'Thomas']
+  },
+  {
+    name: 'Rage',
+    manager: 'Pam',
+    players: ['Cindy', 'Emeshea', 'Ana']
+  },
+  {
+    name: 'Titans',
+    manager: 'Kimi',
+    players: ['Nazar', 'Sylvia', 'Daniel']
+  }
+];
 
 function TeamsList(){
   return (
-    <div>
+    <div style={{backgroundColor: 'white', opacity: '0.8', filter: 'alpha(opacity=50)', padding: '20px'}}>
+      <br></br>
+      <h2 style={{textAlign: 'center'}}>Teams</h2>
+      <br></br>
+      <br></br>
+      <div className="row">
+        {masterTeamsList.map((team, i) =>
+          <Team
+            name={team.name}
+            manager={team.manager}
+            players={team.players}
+            key={i}/>
+        )}
+      </div>
     </div>
   );
 }
