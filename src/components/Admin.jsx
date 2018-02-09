fb07406da4f4b974fee8c75ae71e77870bec99c6
Malgorzata <gosia.haniszewska@gmail.com>
2018-02-09 15:16:49 -0800

    add Admin component

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function Admin(){
   return (
-    <div>
+    <div style={{backgroundColor: 'white', opacity: '0.8', filter: 'alpha(opacity=50)', padding: '20px', textAlign: 'center'}}>
+      <br></br>
+      <br></br>
+      <h3>Admin authentication</h3>
+      <p>This page is only accessible to league administrators. Please log in to make any changes.</p>
+      <br></br>
+      <br></br>
     </div>
   );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,22 +5,22 @@ function Game(props){
   return (
     <div style={{backgroundColor: 'white', opacity: '0.8', filter: 'alpha(opacity=50)', textAlign: 'center', borderStyle: 'solid', borderWidth: '1px 1px 1px 1px', borderColor: 'rgba(1,1,1,1)'}} className="row">
       <div className="col-md-2">
-        <h4>{props.team1}</h4>
+        <h5>{props.team1}</h5>
       </div>
       <div className="col-md-1">
-        <h4>{props.team1Score}</h4>
+        <h5>{props.team1Score}</h5>
       </div>
       <div className="col-md-2">
-        <h4>{props.team2}</h4>
+        <h5>{props.team2}</h5>
       </div>
       <div className="col-md-1">
-        <h4>{props.team2Score}</h4>
+        <h5>{props.team2Score}</h5>
       </div>
       <div className="col-md-3">
-        <h4>{props.date}</h4>
+        <h5>{props.date}</h5>
       </div>
       <div className="col-md-3">
-        <h4>{props.field}</h4>
+        <h5>{props.field}</h5>
       </div>
     </div>
   );
