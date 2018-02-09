f87528c0c250c2b6a33d47084e2f135116695c8f
Malgorzata <gosia.haniszewska@gmail.com>
2018-02-09 14:05:32 -0800

    add PlayersList and Player components

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Shiluette from '../assets/images/shiluette.jpg';
 
-function Player(){
+function Player(props){
   return (
-    <div>
+    <div style={{padding: '50px'}} className="col-md-4">
+      <div className="row">
+        <div className="col-md-4">
+          <img style={{width: '90px'}} src={Shiluette} alt="symbol"></img>
+        </div>
+        <div className="col-md-8">
+          <br></br>
+          <h3>{props.name}</h3>
+        </div>
+      </div>
+      <br></br>
+      <div>
+        <h5><strong>Age: </strong>{props.age}</h5>
+        <h5><strong>Position: </strong>{props.position}</h5>
+        <h5><strong>Team: </strong>{props.team}</h5>
+        <h5><strong>Jersey Number: </strong>{props.jerseyNumber}</h5>
+      </div>
     </div>
   );
 }
+
+Player.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  position: PropTypes.string,
+  team: PropTypes.string,
+  jerseyNumber: PropTypes.number
+};
 
 export default Player;
--- src/components/PlayersList.jsx
+++ src/components/PlayersList.jsx
@@ -1,8 +1,69 @@
 import React from 'react';
+import Player from './Player';
+
+let masterPlayersList= [
+  {
+    name: 'Kevin',
+    age: 27,
+    position: 'Infield',
+    team: 'Expos',
+    jerseyNumber: 10
+  },
+  {
+    name: 'Mike',
+    age: 26,
+    position: '0utfield',
+    team: 'Brewers',
+    jerseyNumber: 3
+  },
+  {
+    name: 'Kam',
+    age: 28,
+    position: 'Pitcher',
+    team: 'Expos',
+    jerseyNumber: 12
+  },
+  {
+    name: 'Tyler',
+    age: 30,
+    position: 'Catcher',
+    team: 'Braves',
+    jerseyNumber: 16
+  },
+  {
+    name: 'Brian',
+    age: 30,
+    position: 'Pitcher',
+    team: 'Expos',
+    jerseyNumber: 5
+  },
+  {
+    name: 'Ana',
+    age: 25,
+    position: 'Catcher',
+    team: 'Expos',
+    jerseyNumber: 7
+  }
+];
 
 function PlayersList(){
   return (
-    <div>
+    <div style={{backgroundColor: 'white', opacity: '0.8', filter: 'alpha(opacity=50)', padding: '20px'}}>
+      <br></br>
+      <h2 style={{textAlign: 'center'}}>Players</h2>
+      <br></br>
+      <br></br>
+      <div className="row">
+        {masterPlayersList.map((player, i) =>
+          <Player
+            name={player.name}
+            age={player.age}
+            position={player.position}
+            team={player.team}
+            jerseyNumber={player.jerseyNumber}
+            key={i}/>
+        )}
+      </div>
     </div>
   );
 }
